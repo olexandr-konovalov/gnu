b6b9decfd8727066336ce3ca59ad63d5f79f9f8e
Alexander Konovalov <alexk@mcs.st-andrews.ac.uk>
2016-05-23 13:09:42 +0100

    Borrowed GnuByConstructAllGroups from lib/gap.sh
    I am not very happy about code duplication, but for now it is useful.

--- grpconst.sh
+++ grpconst.sh
@@ -10,8 +10,68 @@ n := $1;;
 LoadPackage("grpconst");;
 LoadPackage("sglppow");;
 LoadPackage("cubefree");;
+
 IsCubeFree:= n -> ForAll( Collected( Factors(n) ), x -> x[2] <= 2 );;
+
+GnuByConstructAllGroups:=function(n)
+local r, t1, t2, t, tsplit, sublists, gnu, splits, res;
 Print("****************************************\n");
+Print("Constructing all groups of order ", n, "\n");
+Print("****************************************\n");
+res:=rec();
+SetInfoLevel(InfoGrpCon,5);
+t1:=Runtime();
+r:=ConstructAllGroups(n);
+t2:=Runtime();
+t := t2-t1;
+res.result := r;
+res.runtime := t;
+Print("****************************************\n");
+sublists:=Filtered( r, g -> not IsGroup(g) );
+if Length(sublists) = 0 then
+  gnu := Length(r);
+  Print("gnu(" , n , ")=", gnu, "\n\n");
+  Print("GAP      ", GAPInfo.Version, "\n");
+  Print("GrpConst ", GAPInfo.PackagesInfo.grpconst[1].Version, "\n");
+  Print("Runtime: ", t, " ms\n");
+  Print("Isomorphic groups eliminated\n\n");
+  Print("In case this value is new, add the next line to data/gnudata.g\n");
+  Print("GNU_SAVE( ", n, ", ", gnu, ", WITH_GC );\n\n" );
+  Print("****************************************\n");
+else
+  Print("Constructed a list of length ", Length(r), "\n");
+  Print("It contains ", Length(r)-Length(sublists), " groups and ", Length(sublists), " sublists\n");
+  Print("Length of sublists are ", List( sublists, Length ), "\n");
+  if ForAll( sublists, x -> Length(x) = 2 ) then
+    t1:=Runtime();
+    splits := List( sublists, x -> IsomorphismGroups(x[1],x[2]) );
+    t2:=Runtime();
+    tsplit := t2-t1;
+    res.splittime:=tsplit;
+    if ForAll( splits, x -> x = fail ) then
+      Print("Success! All pairs contain non-isomorphic groups\n\n");
+      gnu := Length(r) + Length( sublists );    
+      Print("gnu(" , n , ")=", gnu, "\n\n");
+      Print("GAP      ", GAPInfo.Version, "\n");
+      Print("GrpConst ", GAPInfo.PackagesInfo.grpconst[1].Version, "\n");
+      Print("Runtime of ConstructAllGroups: ", t, " ms\n");
+      Print("Returned a list of length ", Length(r), " containing ", 
+             Length(r)-Length(sublists), " groups and ", Length(sublists), " sublists\n");
+      Print("Length of sublists : ", List( sublists, Length ), "\n");     
+      Print("Runtime to check non-isomorphism of groups in pairs: ", tsplit, "\n\n");
+      Print("In case this value is new, add the next line to data/gnudata.g\n");
+      Print("GNU_SAVE( ", n, ", ", gnu, ", WITH_GC );\n\n" );
+      Print("****************************************\n");
+    else
+      Print("Can not split all pairs - returning all constructed groups for further checks\n");
+    fi;
+  else
+    Print("Long sublists - returning all constructed groups for further checks\n");
+  fi;
+fi;
+return res;
+end;;
+
 if n = 3^8 or IsPrimePowerInt(n) and Length(Factors(n))=7 and Factors(n)[1]>11 then
   Print("Groups of order ", n, " are already in the SglPPow package\n");
   Print("Gnu( " , n , " ) = ", NrSmallGroups( n ), "\n");
@@ -22,24 +82,6 @@ elif IsCubeFree(n) then
   Print(n, " is cubefree. Using 'NumberCFGroups' from the CubeFree package\n");
   Print("Gnu( " , n , " ) = ", NumberCFGroups( n ), "\n");
 else
-  Print("Constructing all groups of order ", n, "\n");
-  Print("****************************************\n");
-  SetInfoLevel(InfoGrpCon,5);;
-  t1:=Runtime();;
-  r:=ConstructAllGroups(n);;
-  t2:=Runtime();;
-  Print("****************************************\n");
-  Print("GAP      ", GAPInfo.Version, "\n");
-  Print("GrpConst ", GAPInfo.PackagesInfo.grpconst[1].Version, "\n");
-  Print("CubeFree ", GAPInfo.PackagesInfo.cubefree[1].Version, "\n");
-  Print("SglPPow  ", GAPInfo.PackagesInfo.sglppow[1].Version, "\n");
-  Print("Runtime: ", t2-t1, " ms\n");
-  if ForAll(r,IsGroup) then
-    Print("Isomorphic groups eliminated\n");
-    Print("gnu(" , n , ") = ", Length(r), "\n");
-  else
-    Print("Constructed a list of length ", Length(r), "\n");
-    Print("but can not check that it is irredundant\n");
-  fi;
+  GnuByConstructAllGroups(n);
 fi;
 EOI
